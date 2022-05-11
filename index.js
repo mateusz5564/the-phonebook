0475e71d84f18585e67ac76ebea51e54a2181857
mateusz5564 <mateusz5564@gmail.com>
2022-05-11 17:46:38 +0200

    the phonebook backend add step8 solution

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'));
 
+morgan.token("body", (req, res) => {
+  if (req.method === "POST") {
+    return JSON.stringify(req.body);
+  } else {
+    return " ";
+  }
+});
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
 let persons = [
   {
